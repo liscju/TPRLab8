a315d6a9494337ad66aa5641c48a173cd3e19298
Bartlomiej Kumor <aargh1@gmail.com>
2015-06-01 08:52:30 +0200

    Added file save, modified update function

--- henrar/main.cpp
+++ henrar/main.cpp
@@ -1,34 +1,27 @@
 #include <iostream>
+#include <cmath>
+#include <fstream>
 
-#DEFINE BSZ (16)
+#define BSZ (16)
 
-void update (float *u, float *u_prev, int N, float h, float dt, float alpha)
+void updateLaplace (float *u, float *u_prev, int N, float h, float dt, float alpha)
 {
-	int i = threadIdx.x;
-	int j = threadIdx.y;
-	int I = blockIdx.y*BSZ*N + blockIdx.x*BSZ + j*N + i;
+
+	int i = 1;
+	int j = 1;
+	int I = 1*BSZ*N + 1*BSZ + j*N + i;
 	
-	if (I>=N*N)
+	if (I >= N*N)
 	{
+		std::cout << "DEBUG: I >= N*N" << std::endl;
 		return;
 	}	
 
-	float u_prev_sh[BSZ][BSZ];
-
-	u_prev_sh[i][j] = u_prev[I];
-	
-	
-	bool bound_check = ((I>N) && (I< N*N-1-N) && (I%N!=0) && (I%N!=N-1)); 
-	bool block_check = ((i>0) && (i<BSZ-1) && (j>0) && (j<BSZ-1));
- 
-	if (block_check)
-	{	
-		u[I] = u_prev_sh[i][j] + alpha*dt/h/h * (u_prev_sh[i+1][j] + u_prev_sh[i-1][j] + u_prev_sh[i][j+1] + u_prev_sh[i][j-1] - 4*u_prev_sh[i][j]);
-	}
-	else if (bound_check) 
+	if ( (I > N) && (I < N*N - 1 - N) && (I % N != 0) && (I % N != N - 1)) 
 	{	
 		u[I] = u_prev[I] + alpha*dt/(h*h) * (u_prev[I+1] + u_prev[I-1] + u_prev[I+N] + u_prev[I-N] - 4*u_prev[I]);
-	} 								}
+		std::cout << "DEBUG: (I > N) && (I < N*N - 1 - N) && (I % N != 0) && (I % N != N - 1)" << std::endl;
+	}			
 } 
 
 int main()
@@ -40,7 +33,7 @@ int main()
 	float xmax 	= 3.5f;
 	float ymin 	= 0.0f;
 	float ymax 	= 2.0f;
-	float h   	= (xmax-xmin)/(N-1);
+	float h   	= (xmax - xmin) / (N - 1);
 	float dt	= 0.00001f;	
 	float alpha	= 0.645f;
 	float time 	= 0.4f;
@@ -51,17 +44,42 @@ int main()
 	float *u  	= new float[N*N];
 	float *u_prev  	= new float[N*N];
 
-	for (int j=0; j<N; j++)
-	{	for (int i=0; i<N; i++)
-		{	I = N*j + i;
+	for (int j = 0; j < N; j++)
+	{	
+		for (int i=0; i<N; i++)
+		{	
+			I = N*j + i;
 			x[I] = xmin + h*i;
 			y[I] = ymin + h*j;
 			u[I] = 0.0f;
-			if ( (i==0) || (j==0)) 
-				{u[I] = 200.0f;}
+			if ( (i == 0) || (j == 0)) 
+			{
+				u[I] = 200.0f;
+			}
 		}
 	}
-	
+	std::cout << "DEBUG: Calling update function" << std::endl;
+	updateLaplace(u, u_prev, N, h, dt, alpha);
+	std::cout << "DEBUG: Exit update function" << std::endl;
+
+	std::ofstream temperature;
+	temperature.open("output/cpu_results.txt");
+	for (int j = 0; j < N; j++)
+	{	
+		for (int i = 0; i < N; i++)
+		{	
+			I = N*j + i;
+			temperature << x[I] << "\t" <<y [I] << "\t" << u[I] << std::endl;
+		}
+		temperature << std::endl;
+	}
+
+	temperature.close();
+
+	delete[] x;
+	delete[] y;
+	delete[] u;
+	delete[] u_prev;
 	
 	return 0;
 }
