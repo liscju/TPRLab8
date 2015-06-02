0dca25b709e917d831988e12649b0d56c37f913e
Sekalski Lukasz <lsekalsk@lab429-01.dydaktyka.icsr.agh.edu.pl>
2015-06-02 12:28:13 +0200

    Updated CPU solution

--- lucas/main.cpp
+++ lucas/main.cpp
@@ -1,35 +1,38 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
+#include <sys/time.h>
 
-#define BSZ (16)
 
-void updateLaplace (float *u, float *u_prev, int N, float h, float dt, float alpha)
-{
+void updateLaplace (float *u, float *u_prev, int N, float h, float dt, float alpha, int BSZ) {
+	for(int i=1; i<N; ++i) {
+		for(int j=1; j<N; ++j) {
+			int I = j*N + i;
 
-	int i = 1;
-	int j = 1;
-	int I = 1*BSZ*N + 1*BSZ + j*N + i;
-	
-	if (I >= N*N)
-	{
-		std::cout << "DEBUG: I >= N*N" << std::endl;
-		return;
-	}	
+			if (I >= N*N) {
+				std::cout << "DEBUG: I >= N*N" << std::endl;
+				continue;
+			}	
 
-	u_prev[I] = u[I];
+			u_prev[I] = u[I];
 
-	if ( (I > N) && (I < N*N - 1 - N) && (I % N != 0) && (I % N != N - 1)) 
-	{	
-		u[I] = u_prev[I] + alpha*dt/(h*h) * (u_prev[I+1] + u_prev[I-1] + u_prev[I+N] + u_prev[I-N] - 4*u_prev[I]);
-		std::cout << "DEBUG: (I > N) && (I < N*N - 1 - N) && (I % N != 0) && (I % N != N - 1)" << std::endl;
-	}			
-} 
+			if ( (I > N) && (I < N*N - 1 - N) && (I % N != 0) && (I % N != N - 1)) {	
+				u[I] = u_prev[I] + alpha*dt/(h*h) * (u_prev[I+1] + u_prev[I-1] + u_prev[I+N] + u_prev[I-N] - 4*u_prev[I]);
+				//std::cout << "DEBUG: (I > N) && (I < N*N - 1 - N) && (I % N != 0) && (I % N != N - 1)" << std::endl;
+			}			
+		}
+	}
+}
 
-int main()
-{
+double get_time() {  
+	struct timeval tim;
+	gettimeofday(&tim, NULL);
+	return (double) tim.tv_sec+(tim.tv_usec/1000000.0);
+}
+
+int main() {
 	int N = 128;
-	int blockSize = BSZ;
+	int blockSize = 16;
 	
 	float xmin 	= 0.0f;
 	float xmax 	= 3.5f;
@@ -46,30 +49,32 @@ int main()
 	float *u  	= new float[N*N];
 	float *u_prev  	= new float[N*N];
 
-	for (int j = 0; j < N; j++)
-	{	
-		for (int i=0; i<N; i++)
-		{	
+	for (int j = 0; j < N; j++) {	
+		for (int i=0; i<N; i++) {	
 			I = N*j + i;
 			x[I] = xmin + h*i;
 			y[I] = ymin + h*j;
 			u[I] = 0.0f;
-			if ( (i == 0) || (j == 0)) 
-			{
+			if ( (i == 0) || (j == 0)) {
 				u[I] = 200.0f;
 			}
 		}
 	}
+	double start = get_time();
 	std::cout << "DEBUG: Calling update function" << std::endl;
-	updateLaplace(u, u_prev, N, h, dt, alpha);
+	for (int t=0; t<steps; t++) {
+		updateLaplace(u, u_prev, N, h, dt, alpha, blockSize);
+	}	
 	std::cout << "DEBUG: Exit update function" << std::endl;
+	double stop = get_time();
+	double elapsed = stop - start;
+	std::cout << "time = " << elapsed << std::endl;
 
 	std::ofstream temperature;
 	temperature.open("output/cpu_results.txt");
-	for (int j = 0; j < N; j++)
-	{	
-		for (int i = 0; i < N; i++)
-		{	
+	temperature << "x[I]\ty[I]\tu[I]" << std::endl;
+	for (int j = 0; j < N; j++) {	
+		for (int i = 0; i < N; i++) {	
 			I = N*j + i;
 			temperature << x[I] << "\t" <<y [I] << "\t" << u[I] << std::endl;
 		}
