264536f7ad9dfcd8aba0477c01cb8589eef297cc
Bartłomiej Kumor <aargh1@gmail.com>
2015-06-07 20:47:50 +0200

    Quick'n'Dirty Windows porting, added TitanX compilation parameters and 64bit options

--- henrar/FD_2D_shared.cu
+++ henrar/FD_2D_shared.cu
@@ -2,7 +2,26 @@
 #include <fstream>
 #include <cmath>
 #include <cstdlib>
-#include <sys/time.h>
+#include <winsock2.h>
+#include <cstdint>
+
+int gettimeofday(struct timeval * tp, struct timezone * tzp)
+{
+	static const uint64_t EPOCH = ((uint64_t)116444736000000000ULL);
+
+	SYSTEMTIME  system_time;
+	FILETIME    file_time;
+	uint64_t    time;
+
+	GetSystemTime(&system_time);
+	SystemTimeToFileTime(&system_time, &file_time);
+	time = ((uint64_t)file_time.dwLowDateTime);
+	time += ((uint64_t)file_time.dwHighDateTime) << 32;
+
+	tp->tv_sec = (long)((time - EPOCH) / 10000000L);
+	tp->tv_usec = (long)(system_time.wMilliseconds * 1000);
+	return 0;
+}
 
 #define BSZ (16)
 
--- henrar/main.cpp
+++ henrar/main.cpp
@@ -2,8 +2,26 @@
 #include <cmath>
 #include <cstdlib>
 #include <fstream>
-#include <sys/time.h>
+#include <winsock2.h>
+#include <cstdint>
 
+int gettimeofday(struct timeval * tp, struct timezone * tzp)
+{
+	static const uint64_t EPOCH = ((uint64_t)116444736000000000ULL);
+
+	SYSTEMTIME  system_time;
+	FILETIME    file_time;
+	uint64_t    time;
+
+	GetSystemTime(&system_time);
+	SystemTimeToFileTime(&system_time, &file_time);
+	time = ((uint64_t)file_time.dwLowDateTime);
+	time += ((uint64_t)file_time.dwHighDateTime) << 32;
+
+	tp->tv_sec = (long)((time - EPOCH) / 10000000L);
+	tp->tv_usec = (long)(system_time.wMilliseconds * 1000);
+	return 0;
+}
 
 void updateLaplace (float *u, float *u_prev, int N, float h, float dt, float alpha) {
 	for(int i=1; i<N; ++i) {
@@ -11,7 +29,6 @@ void updateLaplace (float *u, float *u_prev, int N, float h, float dt, float alp
 			int I = j*N + i;
 
 			if (I >= N*N) {
-				//std::cout << "DEBUG: I >= N*N" << std::endl;
 				continue;
 			}	
 
@@ -19,7 +36,6 @@ void updateLaplace (float *u, float *u_prev, int N, float h, float dt, float alp
 
 			if ( (I > N) && (I < N*N - 1 - N) && (I % N != 0) && (I % N != N - 1)) {	
 				u[I] = u_prev[I] + alpha*dt/(h*h) * (u_prev[I+1] + u_prev[I-1] + u_prev[I+N] + u_prev[I-N] - 4*u_prev[I]);
-				//std::cout << "DEBUG: (I > N) && (I < N*N - 1 - N) && (I % N != 0) && (I % N != N - 1)" << std::endl;
 			}			
 		}
 	}
